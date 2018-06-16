9342578b77f88339ad5b56b012c148d70f3cbfb7
Anders Kostending <aha@systime.dk>
2018-06-16 11:26:50 +0200

    Added vacation counter

---
+++ vacation/script.js
@@ -0,0 +1,18 @@
+$(function() {
+	// Flight time 20. aug kl. 14:55
+	const flightTime = new Date('Aug 20, 2018 14:55:00').getTime();
+
+	let loop = setInterval(function() {
+		const now = new Date().getTime();
+
+		const diff = flightTime - now;
+
+		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+ 		var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  		var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  		var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  		$('#counter').html(days + " dage " + hours + " timer " + minutes + " minutter " + seconds + " sekunder");
+
+	}, 1000);
+});
