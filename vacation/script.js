9a929800bf27aa5cc9fe1b8ab9749a12161f1361
Anders Kostending <aha@systime.dk>
2018-08-16 17:19:04 +0200

    fix for old safari?

--- vacation/script.js
+++ vacation/script.js
@@ -1,11 +1,11 @@
 $(function() {
 	// Flight time 20. aug kl. 14:55
-	const flightTime = new Date('Aug 18, 2018 08:00:00').getTime();
+	var flightTime = new Date('Aug 18, 2018 08:00:00').getTime();
 
-	let loop = setInterval(function() {
-		const now = new Date().getTime();
+	var loop = setInterval(function() {
+		var now = new Date().getTime();
 
-		const diff = flightTime - now;
+		var diff = flightTime - now;
 
 		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
  		var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
