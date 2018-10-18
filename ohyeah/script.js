4b5d5c5cdb0b7db8085fac6bb3a17aead5fbf688
Anders Kostending <aha@systime.dk>
2018-10-18 12:02:02 +0200

    new counter

---
+++ ohyeah/script.js
@@ -0,0 +1,18 @@
+$(function() {
+	var sexTime = new Date('Nov 18, 2018 20:00:00').getTime();
+	console.log(sexTime);
+
+	var loop = setInterval(function() {
+		var now = new Date().getTime();
+
+		var diff = sexTime - now;
+
+		var days = Math.floor(diff / (1000 * 60 * 60 * 24));
+ 		var hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  		var minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  		var seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  		$('#counter').html(days + " Dage " + hours + " Timer " + minutes + " Minutter " + seconds + " Sekunder");
+
+	}, 1000);
+});
